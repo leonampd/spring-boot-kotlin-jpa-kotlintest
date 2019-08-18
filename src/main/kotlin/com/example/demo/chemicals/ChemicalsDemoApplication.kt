cd9190ad8f126ef455022d4dc12abec5e3f1d201
Haikal Saadh <h.saadh@qut.edu.au>
2019-08-18 15:55:06 +1000

    Create new spring boot project with one sample table

---
+++ src/main/kotlin/com/example/demo/chemicals/ChemicalsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo.chemicals
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChemicalsDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChemicalsDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo/chemicals/ChemicalsDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.demo.chemicals
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ChemicalsDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
